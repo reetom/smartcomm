02e1970ec1558634751148b9a7f812be67418ee4
Reetom Hazarika <reetomhazarika@Skavas-MacBook-Pro.local>
2019-05-07 22:05:19 -0700

    used product card in plp

--- src/components/plp.js
+++ src/components/plp.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 import PLPData from './../data/clp.json';
 import landingbanner from '../../src/assets/banners/landingbanner5.jpg';
 import bag2 from '../../src/assets/products/bag2.jpeg';
+import ProductCard from './complibrary/productcard';
 
 
 class PLP extends Component {
@@ -33,30 +34,7 @@ class PLP extends Component {
         //var {products} = this.state;
         console.log(PLPData);
         //loop through every product in the array and build the card.
-        const cardUnit = products.map(product => 
-            <div>
-                <Card shadow={5} style={{minwidth: '200'}}>
-                <CardTitle style={{color: '#fff', height: '250px'}}>
-                <Link to="/pdp">
-                <img
-                src={bag2}
-                alt="bag2"
-                className="card-image"
-                />
-                </Link>
-                </CardTitle>
-                <CardText>
-                    {product.productName} 
-                    ${product.price}
-                </CardText>
-                <CardMenu style={{color: 'RED'}}>
-                    <IconButton name="share" style={{color: 'Blue'}}/>
-                    <IconButton name="favorite" />
-                    <IconButton name="shoppingcart" style={{color: 'Orange'}}/>
-                </CardMenu>
-              </Card>
-            </div>
-        )
+        const cardUnit = products.map(product => <ProductCard product={product}/>)
 
         return(
             <div style={{width: '100%', margin: 'auto'}}>
