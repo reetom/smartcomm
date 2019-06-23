32c401bf62a8772c1fa42799aba2cccda24ab3a7
Reetom Hazarika <reetomhazarika@Skavas-MacBook-Pro.local>
2019-06-23 11:17:56 -0700

    plp uses reactstrap for layout

--- src/components/plp.js
+++ src/components/plp.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import {Grid, Cell, CardText,Card,CardTitle, CardActions, CardMenu, Button, IconButton, Slider, Checkbox} from 'react-mdl';
+import {Button,Slider, Checkbox} from 'react-mdl';
 import CLPData from './../data/clp.json';
 import landingbanner from '../../src/assets/banners/landingbanner5.jpg';
 import BuildProductCard from './complibrary/buildproductcard';
 import Pagination from './complibrary/pagination';
+import {Container, Row, Col} from 'reactstrap';
+import { cpus } from 'os';
 
 
 class PLP extends Component {
@@ -65,70 +67,76 @@ class PLP extends Component {
         const cardUnit = this.state.pageOfItems.map(product =>  <BuildProductCard productFromParent={product}/>)
 
         return(
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className = "plp-landing-grid">
-                    
-                        <Cell col={12}>
-                            <div>
+            <div className="page-background">
+                <Container fluid>
+                    <Row>
+                        <Col sm={12}>
+                        <div>
                                     <img
                                         src={landingbanner}
                                         alt="landingbanner1"
                                         className="plp-landing-banner"
                                     />
-                            </div>
-                        </Cell>
-                        <Cell col={5}>
+                            </div>                   
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={12}>
                             <div>
                                 Breadcrumbs come here @ToDo
                             </div>
-                        </Cell>
-                        <Cell col={7}>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={12}>
                             <div className="text-right">
                                 <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage} />
                             </div>
-                        </Cell>
-                        <Cell col={3}>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={3}>
                             <div className="plp-refine">
-                                <div className="plp-refine-brands">
-                                    <h4>Brands</h4>
-                                    <Checkbox label="Gucci" ripple defaultChecked />
-                                    <Checkbox label="Aldo" ripple defaultChecked />
+                                    <div className="plp-refine-brands">
+                                        <h4>Brands</h4>
+                                        <Checkbox label="Gucci" ripple defaultChecked />
+                                        <Checkbox label="Aldo" ripple defaultChecked />
+                                    </div>
+                                    <div className="plp-refine-color">
+                                        <h4>Colors</h4>
+                                        <Checkbox label="Blue" ripple defaultChecked />
+                                        <Checkbox label="White" ripple defaultChecked />
+                                        <Checkbox label="Maroon" ripple defaultChecked />
+                                        <Checkbox label="Black" ripple defaultChecked />
+                                        <Checkbox label="Yellow" ripple defaultChecked />
+                                        <Checkbox label="Baige" ripple defaultChecked />
+                                        <Checkbox label="Green" ripple defaultChecked />
+                                        <Checkbox label="Brown" ripple defaultChecked />
+                                        <Checkbox label="Orange" ripple defaultChecked />
+                                    </div>
+                                    <div className="plp-refine-color">
+                                        <h4>Material</h4>
+                                        <Checkbox label="Leather" ripple defaultChecked />
+                                        <Checkbox label="Fabric" ripple defaultChecked />
+                                        <Checkbox label="Mixed" ripple defaultChecked />
+                                    </div>
+                                    <div  className="plp-refine-price">
+                                        <h4>Price</h4>
+                                        <Slider min={0} max={5000} defaultValue={2500} />
+                                    </div>
+                                    <div className="plp-refine-button">
+                                        <Button class="plp-button" raised colored onClick={this.handleShowSnackbar}>Apply</Button>
+                                        <Button class="plp-button" raised colored onClick={this.handleShowSnackbar}>Reset</Button>
+                                    </div>
                                 </div>
-                                <div className="plp-refine-color">
-                                    <h4>Colors</h4>
-                                    <Checkbox label="Blue" ripple defaultChecked />
-                                    <Checkbox label="White" ripple defaultChecked />
-                                    <Checkbox label="Maroon" ripple defaultChecked />
-                                    <Checkbox label="Black" ripple defaultChecked />
-                                    <Checkbox label="Yellow" ripple defaultChecked />
-                                    <Checkbox label="Baige" ripple defaultChecked />
-                                    <Checkbox label="Green" ripple defaultChecked />
-                                    <Checkbox label="Brown" ripple defaultChecked />
-                                    <Checkbox label="Orange" ripple defaultChecked />
-                                </div>
-                                <div className="plp-refine-color">
-                                    <h4>Material</h4>
-                                    <Checkbox label="Leather" ripple defaultChecked />
-                                    <Checkbox label="Fabric" ripple defaultChecked />
-                                    <Checkbox label="Mixed" ripple defaultChecked />
-                                </div>
-                                <div  className="plp-refine-price">
-                                    <h4>Price</h4>
-                                    <Slider min={0} max={5000} defaultValue={2500} />
-                                </div>
-                                <div className="plp-refine-button">
-                                <Button class="plp-button" raised colored onClick={this.handleShowSnackbar}>Apply</Button>
-                                <Button class="plp-button" raised colored onClick={this.handleShowSnackbar}>Reset</Button>
-                                </div>
+                        </Col>
+                        <Col sm={9}>
+                            <div  className="plp-grid">
+                                {cardUnit}
                             </div>
-                        </Cell>
-                        <Cell col={9}>
-                        <div  className="plp-grid">
-                            {cardUnit}
-                        </div>
-                    </Cell>
-
-                </Grid>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
